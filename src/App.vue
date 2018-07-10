3c313d6213dc134ef9f1de901dfd34770db37fdc
Raúl Gómez <raul_nouni@hotmail.com>
2018-07-10 07:30:35 -0500

    Curso de Vue.JS, configurando entorno

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  #app
+    p(v-show="showValue") {{ value }}
+    ul
+      li(v-for="i in items") {{ i }}
+    input(v-model="nombre")
+    input(v-model="lastName")
+    p Tu nombre es {{ nombre }}
+    a(:href="url" target="_blank") Nuevo Link
+    p {{ fullName }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Proyecto de VueJS',
+      showValue: true,
+      value: 'Hola Mundo',
+      items: [1, 2, 3, 4, 5],
+      nombre: '',
+      lastName: '',
+      url: 'http://encodedreams.com'
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.nombre} ${this.lastName}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+</style>
