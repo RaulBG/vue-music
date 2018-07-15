8c934b87a812022f9c8902f659e823aed6676a6e
Raúl Gómez <raul_nouni@hotmail.com>
2018-07-15 00:49:47 -0500

    Conceptos basicos

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
     p Tu nombre es {{ nombre }}
     a(:href="url" target="_blank") Nuevo Link
     p {{ fullName }}
+    button(v-on:click="format") Format
+    p {{ formatName }}
 </template>
 
 <script>
@@ -21,12 +23,23 @@ export default {
       items: [1, 2, 3, 4, 5],
       nombre: '',
       lastName: '',
-      url: 'http://encodedreams.com'
+      url: 'http://encodedreams.com',
+      formatName: ''
     }
   },
   computed: {
     fullName () {
       return `${this.nombre} ${this.lastName}`
+    }
+  },
+  watch: {
+    nombre (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+    format () {
+      this.formatName = this.nombre.split(' ').join('-').toUpperCase()
     }
   }
 }
