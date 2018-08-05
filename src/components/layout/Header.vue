101da8aeb320eb8aae4b169cb0bf878b7b8dc85d
Raúl Gómez <raul_nouni@hotmail.com>
2018-08-04 20:07:56 -0500

    Comunicacion entre componentes

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,15 +1,24 @@
 <template lang="pug">
     section.hero.is-info
-        .hero-head
-            header.navbar
-                .container
-                    .navbar-start
-                        .navbar-item
-                            strong 📻 Vue.js Music
-                    .navbar-end.navbar-menu
+      .hero-head
+        header.navbar
+          .container
+            .navbar-start
+              .navbar-item
+                strong 📻 Vue.js Music
+            .navbar-end.navbar-menu
 
         .hero-body
-            .container.has-text-centered
-                h1.title Vue.js Music
-                h2.subtitle Canciones que están Vue-nísimas
+          .container.has-text-centered
+            h1.title Vue.js Music
+            h2.subtitle Canciones que están Vue-nísimas
+            pm-player
+                
 </template>
+<script>
+import PmPlayer from '@/components/Player.vue'
+
+export default {
+  components: { PmPlayer }
+}
+</script>
